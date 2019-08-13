2a7781ee493ae56c6fe78239d492958b421ec01e
zaidakhterr <zaidakhter1202@gmail.com>
2019-08-14 04:33:09 +0500

    Completed site

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}));
+  }
+
+  onSearch = (event) => {
+    this.setState({searchField: event.target.value});
+    console.log(event.target.value);
+  }
+
+  render() {
+    const {robots , searchField} = this.state;
+    const searchedRobot = robots.filter(robots => {
+      return robots.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    console.log(searchedRobot);
+
+    if(!robots.length) {
+      return <h1 className='f1 tc' >Loading ...</h1>
+    } else {
+      return (
+      <div className='tc' >
+        <h1 className='f1' >RoboFriends</h1>
+        <SearchBox searchChange={this.onSearch} />
+        <Scroll>
+          <CardList robots={searchedRobot} />
+        </Scroll>
+      </div>
+    );
+    }
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
 import 'tachyons';
-//import * as serviceWorker from './serviceWorker';
 
-// const element = <Hello hi={'hi react ninja'}/>;
-
-ReactDOM.render(/*element */<Hello hi={'hi react ninja'}/>, document.getElementById('root'));
+ReactDOM.render(<App /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.unregister();
